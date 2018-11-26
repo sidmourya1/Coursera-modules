2dce714166e2aae10ecd6b78cc0f74c8f6795649
Siddharth Mourya <mourya.siddharth@yahoo.in>
2018-11-26 15:13:30 -0600

    Initial Commit for assignment 3

---
+++ Coursera-module-3/NarrowItDownApp.js
@@ -0,0 +1,93 @@
+( function (){
+    'use strict';
+  
+    angular.module('NarrowItDownApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems', FoundItemsDirective)
+    .constant('API_URL', 'https://davids-restaurant.herokuapp.com/');
+  
+    function FoundItemsDirective(){
+      var ddo = {
+        restrict: 'E',
+        templateUrl: 'found-items.template.html',
+        scope:{
+          items: '<',
+          onRemove: '&'
+        },
+        controllerAs: 'list'
+      };
+      return ddo;
+    }
+  
+  
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService){
+      let narrowDownCtrl = this;
+      let menuSearchService = MenuSearchService;
+      let MESSAGE = "Nothing found!";
+  
+      narrowDownCtrl.searchTerm = "";
+      narrowDownCtrl.found = [];
+      narrowDownCtrl.message = "";
+      narrowDownCtrl.messageEnabled = false;
+  
+      narrowDownCtrl.getMatchedMenuItems = function(){
+        narrowDownCtrl.found = [];
+  
+        if(narrowDownCtrl.searchTerm.length === 0){
+          narrowDownCtrl.message = MESSAGE;
+          narrowDownCtrl.messageEnabled = true;
+        }else{
+          console.log("calling service ...");
+          let promise = menuSearchService.getMatchedMenuItems(narrowDownCtrl.searchTerm);
+  
+          promise.then(function success(response){
+            if(response.length === 0){
+              narrowDownCtrl.message = MESSAGE;
+              narrowDownCtrl.messageEnabled = true;
+            }else{
+              narrowDownCtrl.found = response;
+              narrowDownCtrl.messageEnabled = false;
+            }
+            console.log("Elements found: " + narrowDownCtrl.found.length);
+          }).catch(function (error) {
+            console.log("Something went wrong. " + error.message);
+          });
+        }
+      };
+  
+      narrowDownCtrl.removeItem = function(index){
+        narrowDownCtrl.found.splice(index, 1);
+      };
+  
+    }
+  
+    MenuSearchService.$inject = ['$http', 'API_URL'];
+    function MenuSearchService($http, API_URL){
+      let service = this;
+      let items = [];
+  
+      service.getMatchedMenuItems = function(searchTerm){
+        return $http({method: "GET", url: (API_URL + 'menu_items.json') })
+        .then(function success(response){
+          let menuItems = response.data.menu_items;
+          let itemsLength = menuItems.length;
+  
+          for(let i = 0 ; i < itemsLength ; i++ ){
+            if(menuItems[i].description.includes(searchTerm)){
+              items.push(menuItems[i]);
+            }
+          }
+          console.log("Elements retrieved: " + items.length);
+  
+          return items;
+  
+        }).catch(function (error) {
+          console.log("Something went wrong. " + error.message);
+        });
+      };
+  
+    }
+  })();
+  
